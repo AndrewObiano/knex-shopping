54fc0810c620b48077daeb0efdb63758ed96874e
Andrew Obiano <andrew.obiano@gmail.com>
2019-09-14 10:21:56 -1000

    Finished

--- routes/carts.js
+++ routes/carts.js
@@ -3,7 +3,53 @@ const router = express.Router();
 const db = require("../database");
 
 router.get("/", (req, res) => {
-  res.send("Cart page");
+  res.send("Carts page");
+});
+
+router.get("/:user_id", (req, res) => {
+  db.raw(`SELECT product_id FROM carts WHERE user_id = ${req.params.user_id}`)
+    .then(results => {
+      if (results.rows.length === 0) {
+        res.json({ message: "Cart not found!" });
+      } else {
+        res.json(results.rows);
+      }
+    })
+    .catch(err => {
+      res.send(`There is an error! ${err}`);
+    });
+});
+
+router.post("/:user_id/:product_id", (req, res) => {
+  const user_id = req.params.user_id;
+  const product_id = req.params.product_id;
+
+  db.raw(`INSERT INTO carts (user_id, product_id) VALUES(?, ?) RETURNING *`, [
+    user_id,
+    product_id
+  ])
+    .then(results => {
+      res.json({ success: "true" });
+    })
+    .catch(err => {
+      res.send(`There is an error! ${err}`);
+    });
+});
+
+router.delete("/:user_id/:product_id", (req, res) => {
+  const user_id = req.params.user_id;
+  const product_id = req.params.product_id;
+
+  db.raw(`DELETE FROM carts WHERE user_id = ? AND product_id = ? RETURNING *`, [
+    user_id,
+    product_id
+  ])
+    .then(results => {
+      res.json({ success: "true" });
+    })
+    .catch(err => {
+      res.send(`There is an error! ${err}`);
+    });
 });
 
 module.exports = router;
